b4f39ecc4b9a5eb31d33c6caed139798f3844c48
juliomondin <51420121+juliomondin@users.noreply.github.com>
2019-06-10 13:57:38 -0300

    Update FileReaderTests.cs

--- KartSimulatorTests/Infrastructure/FileReaderTests.cs
+++ KartSimulatorTests/Infrastructure/FileReaderTests.cs
@@ -11,12 +11,5 @@ namespace KartSimulatorTests.Infrastructure
         {
             Assert.Throws<FileLoadException>(() => (new FileReader()).GetLines("noFileLocation"));
         }
-
-        //[Fact]
-        //public void GetLines_Returns_File_When_ValidFileName()
-        //{
-        //    var result = new FileReader().GetLines(@"FileReader.cs");
-        //    result.Should().NotBeNullOrEmpty();
-        //}
     }
 }
